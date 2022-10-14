269f5e025963eb7167310962299715c990e06fc7
John <johnlin8968@gmail.com>
2022-10-14 22:58:47 +1100

    Script Creation for  Level 1 Button
    - Added code to load the level scene

---
+++ Assignment3/Assets/Scripts/LevelOne.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelOne : MonoBehaviour
+{
+
+
+    void Start()
+    {
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+}
