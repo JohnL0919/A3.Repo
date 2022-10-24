3e4e136f48cc88b22083c765c8b1301504e6c05e
John <johnlin8968@gmail.com>
2022-10-24 22:26:03 +1100

    Need to Work on Pellet Eaten Sound Effect

--- Assignment3/Assets/Scripts/PelletHandle.cs
+++ Assignment3/Assets/Scripts/PelletHandle.cs
@@ -5,17 +5,32 @@ using UnityEngine;
 public class PelletHandle : MonoBehaviour
 {
     public int pts = 5;
+    public AudioSource Source;
+    public AudioClip Clip;
 
+    public void Start()
+    {
+
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("F1 Car"))
         {
+    
+            gameObject.AddComponent<AudioSource>();
+
             Devour();
+            Source.PlayOneShot(Clip);
         }
     }
 
     protected virtual void Devour()
     {
         FindObjectOfType<PelletManager>().PelletEaten(this);
+
+        
+
     }
+
+
 }
